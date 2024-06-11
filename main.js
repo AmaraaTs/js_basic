be027ac0aebc0e4ec5546dc3947f6cfb36582df8
AmaraaTs <amarjargal.ts01@gmail.com>
2024-06-11 10:54:09 +0800

    java basic

---
+++ main.js
@@ -0,0 +1,92 @@
+// comment
+
+/* olon mor comment bicihed
+ */
+
+/*let ner = "Amaraa"; //string
+let nas = 25; //number 20, 20.3
+let gerleseneseh = true; // boolean: true/false
+let job;
+let angi = null;
+
+console.log("таны нэр: ", ner);
+console.log("таны нас ", nas);
+console.log("таны гэрлэлтийн байдал ", gerleseneseh);
+console.log("таны ажил: ", job);
+console.log("таны анги: ", angi);
+*/
+// typeof
+//operator - vildlvvd
+
+// 2
+let nas = 25;
+
+// 3
+let firstname = "Amarjargal";
+let lastname = "Tsogtbaatar";
+let country = "Mongolia";
+let email = "amarjargal.ts01@gmail.com";
+
+console.log("Таны нас: ", nas);
+console.log("Таны нэр: ", firstname);
+console.log("Таны овог: ", lastname);
+console.log("Таны иргэншил: ", country);
+console.log("Таны цахим хаяг: ", email);
+
+//4
+let anime = "one piece";
+let angi = 1000;
+let bestanime = true;
+
+console.log("anime name: ", anime);
+console.log("anime episode: ", angi);
+console.log("best anime?: ", bestanime);
+console.log("anime name type: ", typeof anime);
+console.log("angi type: ", typeof angi);
+console.log("best anime type: ", typeof bestanime);
+
+//5
+let number1 = 100;
+let number2 = 20;
+console.log("niilber: ", number1 + number2);
+console.log("ylgawar: ", number1 - number2);
+console.log("vrjwer: ", number1 * number2);
+console.log("noogdwor: ", number1 / number2);
+
+let zzz = 0;
+zzz = number1 + number2;
+console.log("nemet niilber: ", zzz);
+
+//6
+let x = 10;
+let y = 13;
+let z = 20;
+console.log("Perimeter", x + y + z);
+
+//7
+let gb = 15;
+let gb_mb = 1000;
+let mb_kb = 1000;
+let kb_bit = 8000;
+console.log("15gb to bit: ", gb * gb_mb * mb_kb * kb_bit);
+console.log("15gb: ", gb * 8e9);
+
+//8
+let C = 10;
+console.log("C to F: ", (C * 9) / 5 + 32);
+
+//9
+let F = 50;
+console.log("F to C: ", ((F - 32) * 5) / 9);
+
+//10
+let too1 = 15;
+let too2 = 10;
+console.log("niilber: ", too1 + too2);
+console.log("ylgawar: ", too1 - too2);
+
+//11
+let cm = 100;
+let inch = 40;
+console.log("cm to inch: ", cm * 0.393701);
+console.log("inch to cm: ", inch / 0.393701);
